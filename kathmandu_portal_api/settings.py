b9ff0bdaa5ed8f0e80d7bf03b1e07b95d373b941
Samyoga Bhattarai <bhattaraisamyoga@gmail.com>
2021-05-08 21:09:20 +0545

    cors issue slved

--- kathmandu_portal_api/settings.py
+++ kathmandu_portal_api/settings.py
@@ -53,6 +53,8 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+CORS_ORIGIN_ALLOW_ALL = True
+
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
