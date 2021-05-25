28759714c284f873695c9c0dd0c44d8eaef0025e
Samyoga Bhattarai <bhattaraisamyoga@gmail.com>
2021-05-25 13:51:12 +0545

    API response time reduce

--- api/models.py
+++ api/models.py
@@ -69,3 +69,17 @@ class BivariateBusinesses (models.Model):
         db_table = 'businesses_bivariate_stats'
         unique_together = (('x_variable', 'x_value', 'y_variable', 'y_value'),)
         managed = False
+
+class BusinessCoordinates (models.Model):
+    m_name_business = models.TextField()
+    value = models.TextField()
+    variable = models.TextField()
+    label_en = models.TextField()
+    label_ne = models.TextField()
+    m_coodinates_latitude = models.TextField()
+    m_coodinates_longitude = models.TextField()
+
+    class Meta:
+        db_table = 'business_coordinate_stats'
+        unique_together = (('m_name_business', 'value', 'variable'))
+        managed = False
--- api/views.py
+++ api/views.py
@@ -22,6 +22,9 @@ class UnivariateViewSet(viewsets.ViewSet):
     filter_backends = (MyFilter,)
 
     def list(Self, request):
+
+        survey = request.GET.get('survey')
+        var_group = request.GET.get('var_group')
        
         # get project root path
         root_path = os.path.dirname(os.path.realpath(__file__))
@@ -29,17 +32,12 @@ class UnivariateViewSet(viewsets.ViewSet):
         variable_title_businesses_csv_file_path = root_path + '/../variable_title_mapping_businesses.csv'
 
         model_univariateworkers = apps.get_model('api', 'UnivariateWorkers')
-        workers_univariate_stats = model_univariateworkers.objects.all()
+        workers_univariate_stats = model_univariateworkers.objects.filter(variable_group__iexact=var_group)
         workers_serializer = getattr(serializers, 'UnivariateWorkers')(workers_univariate_stats, many=True)
 
         model_univariatebusinesses = apps.get_model('api', 'UnivariateBusinesses')
-        businesses_univariate_stats = model_univariatebusinesses.objects.all()
+        businesses_univariate_stats = model_univariatebusinesses.objects.filter(variable_group__iexact=var_group)
         businesses_serializer = getattr(serializers, 'UnivariateBusinesses')(businesses_univariate_stats, many=True)
-
-        survey = request.GET.get('survey')
-        var_group = request.GET.get('var_group')
-        # survey = request.query_params.get('survey')
-        # var_group = request.query_params.get('var_group')
 
         data_labels = ('total', 'perc_of_total', 'label_ne', 'label_en')
         variable_labels = ('variable', 'ques_en', 'ques_ne')
@@ -56,14 +54,12 @@ class UnivariateViewSet(viewsets.ViewSet):
         dist = []
         response = {}
         for data in serializer.data:
-            if data['variable_group']:
-                if data['variable_group'].lower() == var_group:
-                    if data['variable'] not in response:
-                        response[data['variable']] = []
-                    d_labels = {}
-                    for label in data_labels:
-                        d_labels[label] = data[label]
-                    response[data['variable']].append(d_labels)
+            if data['variable'] not in response:
+                response[data['variable']] = []
+            d_labels = {}
+            for label in data_labels:
+                d_labels[label] = data[label]
+            response[data['variable']].append(d_labels)
                     
         final_response = merge_dict(response, title_response)
 
@@ -74,33 +70,30 @@ class UnivariateViewSet(viewsets.ViewSet):
 class BivariateViewSet(viewsets.ViewSet):
     def list(Self, request):
 
+        survey = request.GET.get('survey')
+        var_group = request.GET.get('var_group')
+        dimension = request.GET.get('dimension')
+
         # get project root path
         root_path = os.path.dirname(os.path.realpath(__file__))
         variable_title_workers_csv_file_path = root_path + '/../variable_title_mapping_workers.csv'
         variable_title_businesses_csv_file_path = root_path + '/../variable_title_mapping_businesses.csv'
 
         model_bivariateworkers = apps.get_model('api', 'BivariateWorkers')
-        workers_bivariate_stats = model_bivariateworkers.objects.all()
+        workers_bivariate_stats = model_bivariateworkers.objects.filter(variable_group__iexact=var_group, x_variable=dimension)
         serializer_bivariate_workers = getattr(serializers, 'BivariateWorkers')(workers_bivariate_stats, many=True)
 
         model_bivariatebusinesses = apps.get_model('api', 'BivariateBusinesses')
-        businesses_bivariate_stats = model_bivariatebusinesses.objects.all()
+        businesses_bivariate_stats = model_bivariatebusinesses.objects.filter(variable_group__iexact=var_group, x_variable=dimension)
         serializer_bivariate_businesses = getattr(serializers, 'BivariateBusinesses')(businesses_bivariate_stats, many=True)
 
         model_univariateworkers = apps.get_model('api', 'UnivariateWorkers')
-        workers_univariate_stats = model_univariateworkers.objects.all()
+        workers_univariate_stats = model_univariateworkers.objects.filter(variable__iexact=dimension)
         serializer_univariate_workers = getattr(serializers, 'UnivariateWorkers')(workers_univariate_stats, many=True)
 
         model_univariatebusinesses = apps.get_model('api', 'UnivariateBusinesses')
-        businesses_univariate_stats = model_univariatebusinesses.objects.all()
+        businesses_univariate_stats = model_univariatebusinesses.objects.filter(variable__iexact=dimension)
         serializer_univariate_businesses = getattr(serializers, 'UnivariateBusinesses')(businesses_univariate_stats, many=True)
-
-        survey = request.GET.get('survey')
-        var_group = request.GET.get('var_group')
-        dimension = request.GET.get('dimension')
-        # survey = request.query_params.get('survey')
-        # var_group = request.query_params.get('var_group')
-        # dimension = request.query_params.get('dimension')
 
         serializer_univariate = []
         serializer_bivariate = []
@@ -131,30 +124,27 @@ class BivariateViewSet(viewsets.ViewSet):
         for data in serializer_univariate.data:
             if data['variable'] not in univariate:
                 univariate[data['variable']] = []
-            if data['variable'] == dimension:
-                d_labels = {}
-                for label in data_labels_univariate:
-                    d_labels[label] = data[label]
+            d_labels = {}
+            for label in data_labels_univariate:
+                d_labels[label] = data[label]
 
-                univariate[data['variable']].append(d_labels)
-                univariate_filter[data['variable']] = filter(None, univariate[data['variable']])
+            univariate[data['variable']].append(d_labels)
+            univariate_filter[data['variable']] = filter(None, univariate[data['variable']])
 
         univariate_final = merge_dict(univariate_filter, title_response)
 
         bivariate = {}
         bivariate_filter = {}
         for data in serializer_bivariate.data:
-            if data['variable_group'] and data['x_variable']:
-                if data['variable_group'].lower() == var_group and data['x_variable'] == dimension:
-                    if data['y_variable'] not in bivariate:
-                        bivariate[data['y_variable']] = []
+            if data['y_variable'] not in bivariate:
+                bivariate[data['y_variable']] = []
 
-                    d_labels = {}
-                    for label in data_labels_bivariate:
-                        d_labels[label] = data[label]
+            d_labels = {}
+            for label in data_labels_bivariate:
+                d_labels[label] = data[label]
 
-                    bivariate[data['y_variable']].append(d_labels)
-                    bivariate_filter[data['y_variable']] = filter(None, bivariate[data['y_variable']])
+            bivariate[data['y_variable']].append(d_labels)
+            bivariate_filter[data['y_variable']] = filter(None, bivariate[data['y_variable']])
 
         computed_data = extract_all(bivariate_filter)
                
@@ -229,3 +219,8 @@ def merge_dict(dict1, dict2):
         new_dict['chart_data'] = dict1[key]
         dict3.append(new_dict)
     return dict3
+
+class MapVisualization(viewsets.Viewset):
+    def list(Self, request):
+        
+        
